d6ff711281cb3ed500ba8161b3ee961a10f1418f
Lianny Coringa <lianny_costa@hotmail.com>
2024-04-26 11:50:11 -0300

    create table user

---
+++ src/seeders/20240425141330-users.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
+    [
+      {
+        fullName: 'Manoel Digézio da Costa',
+        email: 'digeziocosta@yahoo.com.br',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'admin',
+        email: 'admin@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {}),
+
+  down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
+};
